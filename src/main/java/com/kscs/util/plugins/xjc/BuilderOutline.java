93e8f57970fc8ec5658f1401e7ac77a286ac34f3
Mirko Klemm <mirko@cm-klemm.de>
2014-03-27 14:26:39 +0100

    Added MIT License to source files

--- src/main/java/com/kscs/util/plugins/xjc/BuilderOutline.java
+++ src/main/java/com/kscs/util/plugins/xjc/BuilderOutline.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2014 Klemm Software Consulting, Mirko Klemm
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.kscs.util.plugins.xjc;
 
 import com.sun.codemodel.ClassType;
@@ -7,7 +30,9 @@ import com.sun.codemodel.JMod;
 import com.sun.tools.xjc.outline.ClassOutline;
 
 /**
- * Created by mirko on 25.03.14.
+ * @author mirko
+ * 25.03.14
+ *
  */
 public class BuilderOutline {
 	private final JDefinedClass definedBuilderClass;
