5020fe446eb279ec1fa9be3c0ad3f0dd45714521
Tim Underwood <timunderwood@gmail.com>
2014-06-04 12:29:36 -0700

    HACK - Make the _RO fields for collections PROTECTED instead of PRIVATE so they work with the Fluent Builder Plugin generated code

--- src/main/java/com/kscs/util/plugins/xjc/ImmutablePlugin.java
+++ src/main/java/com/kscs/util/plugins/xjc/ImmutablePlugin.java
@@ -60,7 +60,7 @@ public class ImmutablePlugin extends Plugin {
 				if(fieldOutline.getPropertyInfo().isCollection() && !((declaredField = PluginUtil.getDeclaredField(fieldOutline)).type().isArray())) {
 					final JClass elementType = ((JClass)declaredField.type()).getTypeParameters().get(0);
 					final JMethod oldGetter = definedClass.getMethod("get"+fieldOutline.getPropertyInfo().getName(true), new JType[0]);
-					final JFieldVar immutableField = definedClass.field(JMod.PRIVATE | JMod.TRANSIENT, declaredField.type(), getImmutableFieldName(declaredField), JExpr._null());
+					final JFieldVar immutableField = definedClass.field(JMod.PROTECTED | JMod.TRANSIENT, declaredField.type(), getImmutableFieldName(declaredField), JExpr._null());
 					definedClass.methods().remove(oldGetter);
 					final JMethod newGetter = definedClass.method(JMod.PUBLIC, oldGetter.type(), oldGetter.name());
 					final JConditional ifFieldNull = newGetter.body()._if(JExpr._this().ref(declaredField).eq(JExpr._null()));
