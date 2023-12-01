2ba5df7199918ff681b8f201913f8d443993e40e
Mihnea Pretorian <mihnea.pretorian@getneteurope.com>
2023-12-01 09:15:30 +0100

    fluent builder getter for a collection property returns empty list in case the builder field is null

--- src/main/java/com/kscs/util/plugins/xjc/PluginContext.java
+++ src/main/java/com/kscs/util/plugins/xjc/PluginContext.java
@@ -114,6 +114,7 @@ public class PluginContext extends Plugin {
 	public static final String COPY_TO_METHOD_NAME = "copyTo";
 	private static final String AS_LIST = "asList";
 	private static final String UNMODIFIABLE_LIST = "unmodifiableList";
+	private static final String EMPTY_LIST = "emptyList";
 	public final JCodeModel codeModel;
 	public final JClass arrayListClass;
 	public final JClass listClass;
@@ -325,6 +326,10 @@ public class PluginContext extends Plugin {
 
 	public JInvocation unmodifiableList(final JExpression expression) {
 		return this.collectionsClass.staticInvoke(PluginContext.UNMODIFIABLE_LIST).arg(expression);
+	}
+
+	public JInvocation emptyList() {
+		return this.collectionsClass.staticInvoke(PluginContext.EMPTY_LIST);
 	}
 
 	public boolean hasPlugin(final Class<? extends Plugin> pluginClass) {
